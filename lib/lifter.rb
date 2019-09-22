0309738f81f6fb95be576033ecb37dfe0e9b509b
Tim Golding <tgolding55@hotmail.com>
2019-09-22 18:23:40 +0100

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,35 @@
 class Lifter
+  @@all = []
   attr_reader :name, :lift_total
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select{|membership| membership.lifter == self}
+  end
+
+  def gym_memberships
+    memberships.map{|membership| membership.gym}
+  end
+
+  def self.average_total
+    self.all.sum{|lifter| lifter.lift_total} / self.all.length
+  end
+
+  def memberships_cost
+    memberships.sum {|membership| membership.cost}
+  end
+
+  def join_gym(gym, cost)
+    Membership.new(cost, self, gym)
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,20 @@ require_relative '../config/environment.rb'
 
 # test code goes here
 
+lifter1 =  Lifter.new("lifter1", 10)
+lifter2 =  Lifter.new("lifter2", 10)
+lifter3 =  Lifter.new("lifter3", 100)
+lifter4 =  Lifter.new("lifter4", 0)
+
+gym1 = Gym.new("gym1")
+gym2 = Gym.new("gym2")
+gym3 = Gym.new("gym3")
+
+membership1 = Membership.new(20, lifter1, gym1)
+membership2 = Membership.new(1, lifter2, gym1)
+membership3 = Membership.new(200, lifter2, gym2)
+membership4 = Membership.new(15, lifter3, gym1)
+
 binding.pry
 
 puts "Gains!"
