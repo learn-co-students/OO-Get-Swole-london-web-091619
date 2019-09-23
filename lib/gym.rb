a63266637047f266c1b7916ed1a17774e855773a
thetamicodes <reichold.tamara@gmail.com>
2019-09-23 23:34:42 +0100

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,31 @@
 class Gym
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  #get list of all memberships at specific gym
+  def memberships
+    Membership.all.select { |membership| membership.gym == self }
+  end
+
+  #lifters with membership at specific gym
+  def lifters
+    Membership.all.select { |lifter| lifter.gym == self }
+  end
+
+  #list of names of all lifters that have membership to that gym
+  def lifters_names
+    lifters.map { |lifter| lifter.name }
+  end
+
 end
+
--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,39 @@
 class Lifter
   attr_reader :name, :lift_total
 
+  @@all = []
+
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  #list of all memberships a specific lister has
+  def memberships
+    Membership.all.select { |membership| membership.lifter == self }
+  end
+
+  #list of all gyms that a specific lifter has memberships to
+  def gyms
+    Membership.all.select { |gym| gym.lifter == self }
+  end
+
+  #average lift total of all lifters
+  def self.average_lift_total
+    lift_list = self.all.map {|lifter| lifter.lift_total }
+    lift_list.sum / lift_list.length
+  end
+
+  #total costs of a specific lifter's gym memberships
+  def total_costs
+    Membership.all.select { |cost| cost.lifter == self }
+  end
+
 end
+
+
